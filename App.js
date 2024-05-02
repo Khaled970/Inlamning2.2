1c54dc1e77da74de9238a8baa68220648828a1e8
Khaled97 <khalilmaleki97@gmail.com>
2024-05-02 19:01:43 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,5 @@
+function App(){
+    const [yourName] = useState("Khalil");
+    return <h1>Hello {yourName}</h1>;
+}
+export default App;
---
+++ Script.js
@@ -0,0 +1,32 @@
+
+function myFunction(){
+    document.getElementById("myApp").classList.toggle("show");
+}
+window.onclick = function(event){
+    if(!event.target.matches('.apppro')){
+        var dropdowns = document.getElementsByClassName("Portfolio-con");
+        var i;
+        for(i = 0; i < dropdowns.length; i++){
+            var openDropdown = dropdowns[i];
+            if(openDropdown.classList.contains('show')){
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+
+var dropdown = document.getElementsByClassName("Logginbtn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
